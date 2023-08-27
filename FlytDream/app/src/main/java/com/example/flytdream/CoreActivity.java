4764433757e0800e129cbca0b74825c58a9be587
VietThaiNguyen <142501692+VietThaiNguyen@users.noreply.github.com>
2023-08-28 01:09:09 +1000

    Bottom Navigation Menu
    This commit creates the Navigation Menu at the bottom of CoreActivity to navigate through different section of the application.

--- FlytDream/app/src/main/java/com/example/flytdream/CoreActivity.java
+++ FlytDream/app/src/main/java/com/example/flytdream/CoreActivity.java
@@ -1,14 +1,40 @@
 package com.example.flytdream;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class CoreActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_core);
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(navigationMenuListener);
     }
+
+    NavigationBarView.OnItemSelectedListener navigationMenuListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if (item.getItemId() == R.id.menu_home) {
+                return true;
+            } else if (item.getItemId() == R.id.menu_trip) {
+                return true;
+            } else if (item.getItemId() == R.id.menu_explore) {
+                return true;
+            } else if (item.getItemId() == R.id.menu_profile) {
+                return true;
+            }
+            return false;
+        }
+    };
 }
