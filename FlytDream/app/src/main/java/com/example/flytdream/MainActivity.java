844a2a5a364eea00b97ce789971c45bb14125e64
VietThaiNguyen <142501692+VietThaiNguyen@users.noreply.github.com>
2023-08-29 19:16:04 +1000

    Launce Screen
    This commit implement the launch screen of the application.

--- FlytDream/app/src/main/java/com/example/flytdream/MainActivity.java
+++ FlytDream/app/src/main/java/com/example/flytdream/MainActivity.java
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton getStartButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, CoreActivity.class);
-        startActivity(intent);
+        getStartButton = findViewById(R.id.getStartButton);
+        getStartButton.setOnClickListener(buttonClickListener);
     }
+
+    View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.getStartButton) {
+                Intent intent = new Intent(MainActivity.this, CoreActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
