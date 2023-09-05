019ddd85a52c465283d32b2ed6f55ed9f9996e32
VietThaiNguyen <142501692+VietThaiNguyen@users.noreply.github.com>
2023-09-05 22:11:19 +1000

    Database
    This commit implement a database to record the users and cities.

--- FlytDream/app/src/main/java/com/example/flytdream/MainActivity.java
+++ FlytDream/app/src/main/java/com/example/flytdream/MainActivity.java
@@ -8,15 +8,32 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    databaseHelper myDB;
     ImageButton getStartButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myDB = new databaseHelper(MainActivity.this, "FlytDream.db", null, 1);
+
         getStartButton = findViewById(R.id.getStartButton);
         getStartButton.setOnClickListener(buttonClickListener);
+    }
+
+    private void loadCities() {
+        if (!myDB.tableHasContent(databaseHelper.CITY_INFO_TABLE)) {
+            myDB.insertCity("AMS","Amsterdam","Netherland");
+            myDB.insertCity("HAN","Hanoi","Viet Nam");
+            myDB.insertCity("HCM","Ho Chi Minh City","Viet Nam");
+            myDB.insertCity("LDN","London","United Kingdom");
+            myDB.insertCity("MEL","Melbourne","Australia");
+            myDB.insertCity("PAR","Paris","France");
+            myDB.insertCity("BEI","Beijing","China");
+            myDB.insertCity("PER","Perth","Australia");
+            myDB.insertCity("SYD","Sydney","Australia");
+            myDB.insertCity("TKY","Tokyo","Japan");
+        }
     }
 
     View.OnClickListener buttonClickListener = new View.OnClickListener() {
---
+++ FlytDream/app/src/main/java/com/example/flytdream/databaseHelper.java
@@ -0,0 +1,105 @@
+package com.example.flytdream;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class databaseHelper extends SQLiteOpenHelper {
+    //Fields
+    public static final String DATABASE_NAME = "FlytDream.db";
+
+    public static final String USER_INFO_TABLE = "USER_INFO";
+    public static final String USER_INFO_TABLE_COL_1 = "USER_NAME";
+    public static final String USER_INFO_TABLE_COL_2 = "USER_EMAIL";
+    public static final String USER_INFO_TABLE_COL_3 = "USER_PASSWORD";
+    public static final String USER_INFO_TABLE_COL_4 = "USER_PHONE";
+
+    public static final String CITY_INFO_TABLE = "CITY_INFO";
+    public static final String CITY_INFO_TABLE_COL_1 = "CITY_ALIAS";
+    public static final String CITY_INFO_TABLE_COL_2 = "CITY_NAME";
+    public static final String CITY_INFO_TABLE_COL_3 = "CITY_COUNTRY";
+
+    //Constructor
+    public databaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //Method onCreate() create the databases if they don't exist
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + USER_INFO_TABLE + "(USER_NAME TEXT PRIMARY KEY, USER_EMAIL TEXT, USER_PASSWORD TEXT, USER_PHONE TEXT)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + CITY_INFO_TABLE + "(CITY_ALIAS TEXT PRIMARY KEY, CITY_NAME TEXT, CITY_COUNTRY TEXT)");
+    }
+
+    //Method onUpgrade() upgrade the database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + USER_INFO_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + CITY_INFO_TABLE);
+        onCreate(db);
+    }
+
+    //Method tableHasContent() checks if the table already has content before insert
+    public boolean tableHasContent(String tableName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + tableName, null);
+        if (!res.moveToFirst()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //Method insertUser() inserts the user into the database
+    public void insertUser(String name, String email, String password, String phone) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(USER_INFO_TABLE_COL_1, name);
+        contentValues.put(USER_INFO_TABLE_COL_2, email);
+        contentValues.put(USER_INFO_TABLE_COL_3, password);
+        contentValues.put(USER_INFO_TABLE_COL_4, phone);
+
+        db.insert(USER_INFO_TABLE, null, contentValues);
+    }
+
+    //Method checkUserExistence() check if the user exist in the database
+    public boolean checkUserExistence(String name) {
+        boolean userExist = false;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + USER_INFO_TABLE + " WHERE USER_NAME=?", new String[]{name});
+        if (res.moveToFirst()) {
+            userExist = true;
+        }
+        res.close();
+        return userExist;
+    }
+
+    //Method checkPasswordMatch() checks if the password match
+    public boolean checkPasswordMatch(String name, String inputPassword) {
+        boolean passwordMatch = false;
+        String password = "";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + USER_INFO_TABLE + " WHERE USER_NAME=?", new String[]{name});
+        while (res.moveToNext()) {
+            password = res.getString(2);
+        }
+        if (password.equals(inputPassword)) {
+            passwordMatch = true;
+        }
+        res.close();
+        return passwordMatch;
+    }
+
+    public void insertCity(String cityAlias, String cityName, String cityCountry) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(CITY_INFO_TABLE_COL_1, cityAlias);
+        contentValues.put(CITY_INFO_TABLE_COL_2, cityName);
+        contentValues.put(CITY_INFO_TABLE_COL_3, cityCountry);
+        db.insert(CITY_INFO_TABLE, null, contentValues);
+    }
+}
